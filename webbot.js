461a641cfd357f23d03d6a74d57659aaf6ade2e4
Larry1123 <larry1123@larry1123.net>
2016-02-08 00:55:58 -0500

    Webbot should do the thing now :)

--- webbot.js
+++ webbot.js
@@ -22,50 +22,53 @@ app.get('/', function(req, res) {
 app.get('/auth/twitch/', function (req, res) {
     var send = '';
     if (req.query.code && req.query.scope) {
-        var accessToken = req.query.code;
-        twitchManager.getThisUser(accessToken, function (err, body) {
-            if (err) {
-                console.error(err);
-                send += 'getThisUser\n';
-                res.send(send + ' ' + err.status + ':' + err.error + ': ' + err.message);
-                return;
-            }
-            if (body) {
-                // ok we know that they have token and a name
-                var user = body.display_name;
-                var key = crypto.createHash('md5').update(user).digest('hex');
-                redisManager.getTwitchAuthKey(key, function (err, result) {
-                    if (err) {
-                        console.error(err);
-                        send += 'getTwitchAuthKey\n';
-                        res.send(send + ' ' + err.status + ':' + err.error + ': ' + err.message);
-                        return;
-                    }
-                    // see if we need to save this key.
-                    if (!result) {
-                        redisManager.setTwitchAuthKey(key, user);
-                    }
-                    // See if they are a sub
-                    twitchManager.getChannelSubscriptionOfUser(user, accessToken, function (err, body2) {
-                        send += 'Use "!twitchlink ' + key + '" in the dubtrack chat to get link your dubtrack to your twitch.\n';
-                        if (err && err.status == 404) {
-                            res.send("You are not a twitch sub!");
-                        }
-                        else if (err) {
+        var code = req.query.code;
+        twitchManager.getAccessToken(code, function(err, body) {
+            var accessToken = body.access_token;
+            twitchManager.getAuthenticatedUser(accessToken, function (err, body) {
+                if (err) {
+                    console.error(err);
+                    send += 'getThisUser\n';
+                    res.send(send + ' ' + err.status + ':' + err.error + ': ' + err.message);
+                    return;
+                }
+                if (body) {
+                    // ok we know that they have token and a name
+                    var user = body.display_name;
+                    var key = crypto.createHash('md5').update(user).digest('hex');
+                    redisManager.getTwitchAuthKey(key, function (err, result) {
+                        if (err) {
                             console.error(err);
-                            send += 'getChannelSubscriptionOfUser\n';
+                            send += 'getTwitchAuthKey\n';
                             res.send(send + ' ' + err.status + ':' + err.error + ': ' + err.message);
                             return;
                         }
-                        if (body2) {
-                            send += 'Your a Twitch sub! When you use the !twitchlink command it will make any needed change.\n';
-                            send += 'Just note if you have a staff role as it is it will not change your role.\n';
-                            redisManager.setTwitchSub(user, true);
+                        // see if we need to save this key.
+                        if (!result) {
+                            redisManager.setTwitchAuthKey(key, user);
                         }
-                        res.send(send);
+                        // See if they are a sub
+                        twitchManager.getChannelSubscriptionOfUser(user, accessToken, function (err, body) {
+                            send += 'Use "!twitchlink ' + key + '" in the dubtrack chat to get link your dubtrack to your twitch.\n';
+                            if (err && err.status == 404) {
+                                res.send("You are not a twitch sub!");
+                            }
+                            else if (err) {
+                                console.error(err);
+                                send += 'getChannelSubscriptionOfUser\n';
+                                res.send(send + ' ' + err.status + ':' + err.error + ': ' + err.message);
+                                return;
+                            }
+                            if (body) {
+                                send += 'Your a Twitch sub! When you use the !twitchlink command it will make any needed change.\n';
+                                send += 'Just note if you have a staff role as it is it will not change your role.\n';
+                                redisManager.setTwitchSub(user, true);
+                            }
+                            res.send(send);
+                        });
                     });
-                });
-            }
+                }
+            });
         });
     }
     else {
