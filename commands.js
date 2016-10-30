6d6017f4473fbd4bde4b15d280b55be0046cf968
BuildTools <martirod055@gmail.com>
2016-10-30 17:31:10 -0300

    Changed a bit !porps message PART II I "ate a word" :x

--- commands.js
+++ commands.js
@@ -142,7 +142,7 @@ function regCommands(commandManager) {
              */
             function (utils) {
                 utils.bot.sendChat('@' + utils.getUserUsername() + ' here you go, a porpoise: https://i.imgur.com/1oBtXoN.png ' +
-                    '_...or if you !props, then you might wanna retry with the correct spelling._');
+                    '_...or if you meant !props, then you might wanna retry with the correct spelling._');
             }
         )
         ,
